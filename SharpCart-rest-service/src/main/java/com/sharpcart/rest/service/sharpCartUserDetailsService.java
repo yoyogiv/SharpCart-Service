9f52207ee2d6c7756b10cee7b34fc432a893190d
yoyogiv <yoram.givon@gmail.com>
2014-03-22 21:01:57 -0500

    working on getting spring security to work

--- SharpCart-rest-service/src/main/java/com/sharpcart/rest/service/sharpCartUserDetailsService.java
+++ SharpCart-rest-service/src/main/java/com/sharpcart/rest/service/sharpCartUserDetailsService.java
@@ -6,23 +6,29 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import com.sharpcart.rest.controller.UserManagementController;
 import com.sharpcart.rest.model.sharpCartUserDetails;
 import com.sharpcart.rest.persistence.model.SharpCartUser;
 
 @Component ("userService")
 public class sharpCartUserDetailsService implements UserDetailsService {
-
+	private static Logger LOG = LoggerFactory.getLogger(sharpCartUserDetailsService.class);
 	private SessionFactory factory;
 	private Session session;
 
 	@Override
 	public UserDetails loadUserByUsername(String username)
 			throws UsernameNotFoundException {
+		
+		//debug
+		LOG.info("User Name: "+username); //name
 		
 		//Find user in database
 		Configuration configuration = new AnnotationConfiguration ().configure();
